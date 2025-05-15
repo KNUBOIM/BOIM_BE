f61955a02ce34bd376e66f4465c19d10dc738114
unknown <kim0615157@gmail.com>
2025-05-16 03:32:03 +0900

    feat: jwt filter

---
+++ src/main/java/BOIM_Project/boim/auth/security/JwtAuthenticationFilter.java
@@ -0,0 +1,48 @@
+package BOIM_Project.boim.auth.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+        if (StringUtils.hasText(header) && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            try {
+                Jws<Claims> claims = jwtTokenProvider.validateToken(token);
+                String email = claims.getBody().getSubject();
+                String role = claims.getBody().get("role", String.class);
+                GrantedAuthority grantedAuthority = () -> role;
+                Authentication authentication = new UsernamePasswordAuthenticationToken(email, null,
+                    List.of(grantedAuthority));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (Exception e) {
+                SecurityContextHolder.clearContext();
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
