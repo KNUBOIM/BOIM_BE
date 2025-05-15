7d72f8738a3bb34d27fd7fd22742e5d9fd745958
unknown <kim0615157@gmail.com>
2025-05-16 00:33:31 +0900

    Initial commit

---
+++ src/main/java/BOIM_Project/boim/BoimApplication.java
@@ -0,0 +1,13 @@
+package BOIM_Project.boim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoimApplication.class, args);
+	}
+
+}
